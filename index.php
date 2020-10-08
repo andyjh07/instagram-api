576214f999c9e346e6906f9a72bdfafe883547d0
Andy Holmes <info@andyholmes.me>
2020-10-08 01:19:13 +0100

    Update index.php
    brought feed back

--- index.php
+++ index.php
@@ -3,8 +3,7 @@
 require 'vendor/autoload.php';
 
 $instagram = new App\Instagram("IGQVJYYUpzZA043UnJtVEpyZA0UwT1pfWWROQTczZA3Q3ZAUM0UTRhTW9iR2RoNFFSbHZAyYTFnanNWTVZArUjhoaUlxQngxNFNqU3l1THp4T2p0RGE5Sk1kUlRvQVNDY1dVQTRRLU5JQzB3");
-// $items = $instagram->feed(6, ['IMAGE', 'CAROUSEL_ALBUM']);
-$instagram->renewToken();
+$items = $instagram->feed(6, ['IMAGE', 'CAROUSEL_ALBUM']);
 
 foreach($items as $item){
   echo $item->media_url;
