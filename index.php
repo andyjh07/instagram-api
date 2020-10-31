9c1fc2f73e9c3dfbf42cddf6e79b2514c2045f6d
Andy Holmes <andy@pillorybarn.co.uk>
2020-10-31 20:56:04 +0000

    Update index.php
    updated basic layout, need to add some actual styling though

--- index.php
+++ index.php
@@ -4,8 +4,13 @@ require 'vendor/autoload.php';
 
 $instagram = new App\Instagram("IGQVJVU1JGcHo4RDhsNm5TUGxHdjFOVWkyZAWpzclJGLThpZAFJXS1hfRFJxX2xvd0FCaWttVlZAhdHBRcS1MRE1oWTBjemlycUtweVp0MnROYjkxcGtJbkEtUllDYzdHbktaMU1PLWZAhTVhFT3E0aDBMbwZDZD");
 $items = $instagram->getFeed(6, ['IMAGE', 'CAROUSEL_ALBUM']);
-
-foreach($items as $item){
-  echo "<a href='{$item->permalink}' target='_blank'><img src='{$item->media_url}'></a>";
-}
 ?>
+
+<div id="instagramFeed">
+  <?php
+    foreach($items as $item){
+      $item->caption = trim(substr($item->caption, 0, 60));
+      echo "<a target='_blank' href='{$item->permalink}'><img src='{$item->media_url}' alt=''><span>@{$item->username}: {$item->caption}...</span></a>";
+    }
+  ?>		
+</div>
