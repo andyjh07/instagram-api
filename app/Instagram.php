a331094efa141debb6d9faec7d6cdaef833721ec
Andy Holmes <andy@pillorybarn.co.uk>
2021-01-12 10:30:48 +0000

    More little updates

--- app/Instagram.php
+++ app/Instagram.php
@@ -12,10 +12,12 @@ class Instagram {
 
   public function __construct()
   {
+    // Set up the Guzzle instance and set the base uri for all calls
     $this->client = new Guzzle([
       'base_uri' => 'https://graph.instagram.com/'
     ]);
-
+    
+    // Read token from file, could easily be changed to read from a database
     $this->token = file_get_contents($this->file);
   }
 
@@ -24,6 +26,7 @@ class Instagram {
    */
   public function getFeed(int $limit, array $types = [])
   {
+    // Query the media for the user with the values we pass through
     $res = $this->client->request('GET', 'me/media', [
       'query' => [
         'fields' => 'id,caption,permalink,media_type,media_url,username,timestamp',
@@ -36,7 +39,6 @@ class Instagram {
      * If query is successful, clean out any media types that aren't required and return to the user
      */
     if($res->getStatusCode() == 200){
-    
       $feed = json_decode($res->getBody());
       $cleanFeed = [];
     
@@ -70,7 +72,7 @@ class Instagram {
       $token = $this->token;
     }
 
-    // Save token to .dat file
+    // Save token to .dat file. This could be changed to update a database table
     file_put_contents($this->file, $token);
   }
 }
