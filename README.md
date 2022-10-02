3274004713b77c033e3a6d37b17e06966077d1c6
Andy Holmes <info@andyholmes.me>
2022-10-02 11:12:28 +0100

    Frontend
    Added html5 structure, installed tailwind 3, setup input/output files

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # instagram-api
 
 This project is just a quick script I put together to utilise the Instagram API in PHP in a fairly simple way. It needs more work to store the token in a .env file (or similar) but it does the job it was built for at the minute. Once you get your Instagram API token, add it to an `instagram.dat` file in the root of your project, then take a look at the example in `index.php`. There's also a `renewToken` method to renew the token (obviously) once the endpoit is hit, so setting a cron to access this endpoint every 28 days should keep your instagram feed alive.
+
+You can follow this tutorial on how to generate an access token for your API - https://docs.oceanwp.org/article/487-how-to-get-instagram-access-token
--- index.php
+++ index.php
@@ -1,17 +1,25 @@
 <?php
+  require 'vendor/autoload.php';
 
-require 'vendor/autoload.php';
-
-$instagram = new App\Instagram();
-$items = $instagram->getFeed(6, ['IMAGE', 'CAROUSEL_ALBUM']);
-
+  $instagram = new App\Instagram();
+  $items = $instagram->getFeed(6, ['IMAGE', 'CAROUSEL_ALBUM']);
 ?>
-
-<div id="instagramFeed">
-  <?php
-    foreach($items as $item){
-      $item->caption = trim(substr($item->caption, 0, 60));
-      echo "<a target='_blank' href='{$item->permalink}'><img src='{$item->media_url}' alt=''><span>@{$item->username}: {$item->caption}...</span></a>";
-    }
-  ?>		
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Basic Instagram API library</title>
+</head>
+<body>
+  <div id="instagramFeed">
+    <?php
+      foreach($items as $item){
+        $item->caption = trim(substr($item->caption, 0, 60));
+        echo "<a target='_blank' href='{$item->permalink}'><img src='{$item->media_url}' alt=''><span>@{$item->username}: {$item->caption}...</span></a>";
+      }
+    ?>		
+  </div>
+</body>
+</html>
