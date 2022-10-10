d826c1298af49577459602fb7dc05ce42910bdf8
Andy Holmes <info@andyholmes.me>
2022-10-10 12:14:21 +0100

    Added tailwind
    installed Tailwind 3.1 to example and output example feed into a grid.

--- index.php
+++ index.php
@@ -11,13 +11,15 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Basic Instagram API library</title>
+  
+  <link rel="stylesheet" href="dist/output.css">
 </head>
 <body>
-  <div id="instagramFeed">
+  <div id="instagramFeed" class="grid grid-cols-12 gap-y-6 gap-x-6">
     <?php
       foreach($items as $item){
         $item->caption = trim(substr($item->caption, 0, 60));
-        echo "<a target='_blank' href='{$item->permalink}'><img src='{$item->media_url}' alt=''><span>@{$item->username}: {$item->caption}...</span></a>";
+        echo "<a class='col-span-2' target='_blank' href='{$item->permalink}'><img src='{$item->media_url}' alt=''><span>@{$item->username}: {$item->caption}...</span></a>";
       }
     ?>		
   </div>
